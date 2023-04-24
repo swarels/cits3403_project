ceaf4bf670103e9a854aa9d9dc7d1fe3ac2d9a53
anupa.benoy <anupabenoy7@gmail.com>
2023-04-24 18:33:50 +0800

    basic folders and files needed to create chat room

---
+++ main.py
@@ -0,0 +1,21 @@
+# install following before beginning:
+# pip install flask
+# pip install flask_socketio
+
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["secretKey"] = "secretkey123"
+socketio = SocketIO(app)
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
