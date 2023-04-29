43d426a50afd60cfb161d0715f56d4dcb227eb8d
swarels <104413979+swarels@users.noreply.github.com>
2023-04-29 13:18:13 +0800

    signups added to homepage

--- main.py
+++ main.py
@@ -7,14 +7,31 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
+from forms import SignUpForm
+
 app = Flask(__name__)
-app.config["secretKey"] = "secretkey123"
+app.config["SECRET_KEY"] = "secretkey123"
 socketio = SocketIO(app)
 
-
+"""
+Main homepage
+"""
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("home.html")
+
+
+"""
+Signup page
+"""
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    # if form.is_submitted():
+    #     result = request.form
+    #     return 
+    return render_template("signup.html", form = form)
+
 
 
 if __name__ == "__main__":
