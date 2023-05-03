8ddb4c2fec5f647b0d47a1ea77fe0c268c93d8bc
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-03 13:50:32 +0800

    Logging in now generates flash message

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
-app = Flask(__name__)
-app.config["secretKey"] = "secretkey123"
-socketio = SocketIO(app)
+from forms import LoginForm
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretkey123"
+socketio = SocketIO(app)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html")
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user (), remember=()'.format(
+            form.username.data, form.remember.data))
+        return redirect('/room')
+    return render_template("home.html", title='Sign In', form=form)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
