7c58c35bb48cc570702fe2c75f9c932369f09bf9
anupa.benoy <anupabenoy7@gmail.com>
2023-05-13 13:25:52 +0800

    added rat pic to sign up page, created test file

--- tests.py
+++ tests.py
@@ -1,5 +1,12 @@
-import unittest
-# import what file you want to test here 
+import unittest, os
+from <DATABASE> import <DATABASE>, db 
+from <DATABASE>.models import <TABLES TO TEST>
 
 class TestDB(unittest.TestCase):
-    def test_
+    def setUp(self):
+        basedir = #enter path
+
+
+
+
+
