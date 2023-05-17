e021d73c77852849c56b0c671042704b09766d00
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-17 13:08:57 +0800

    Created working "app" package

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # pip install flask-sqlalchemy
 # pip install flask-migrate
 
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, request, session, redirect
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -15,7 +15,7 @@ from flask_wtf.csrf import CSRFProtect
 from wtforms import TextAreaField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-from app.forms import LoginForm
+#from app.forms import LoginForm
 #from app.forms import SignUpForm
 
 from config import Config
@@ -34,42 +34,16 @@ app = Flask(__name__)
 #app.config['WTF_CSRF_SECRET_KEY'] = "verysecretKEY"
 #app.config['SESSION_TYPE'] = "filesystem"
 
-socketio = SocketIO(app)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#socketio = SocketIO(app)
+#app.config.from_object(Config)
+#db = SQLAlchemy(app)
+#migrate = Migrate(app, db)
 
-from app import routes, models
-
-# """
-# Initialize the database
-# """
-# mydb = mysql.connector.connect(
-#     host = "localhost",
-#     user="root",
-#     password="23073177"
-# )
-# # print(mydb) # tests the connection
-# mycurser = mydb.cursor()
-# mycurser.execute("CREATE DATABASE GymUsers")
-
+from app import app
 
 """
 Main homepage
 """
-@app.route("/", methods=["POST", "GET"])
-@app.route("/index", methods=["POST", "GET"])
-def home():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user (), remember=()'.format(
-            form.username.data, form.remember.data))
-        return redirect('/room')
-    return render_template("home.html", title='Sign In', form=form)
-
-@app.route("/room")
-def room():
-    return render_template("room.html")
 
 """
 Signup page
