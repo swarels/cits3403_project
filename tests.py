77e4b01be8648a3d43a1755a28bdc1422b32a1ef
anupa.benoy <anupabenoy7@gmail.com>
2023-05-17 13:53:38 +0800

    fixing links and working on db test

--- tests.py
+++ tests.py
@@ -9,7 +9,19 @@ class TestDB(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = \'sqlite:///' + os.path.join(basedir, 'test.db')
         self.app = app_test_client()
         db.create_all()
-        
+        u1 = User(username="user1", name="Test Case", hashed_password="testpassword", messages="test message 1", is_pro="N")
+        u2 = User(username="user2", name="Test Case2", hashed_password="testpassword2", messages="test message 2", is_pro="Y")
+        m1 = Message(id=1, text="testmessage 1", time="IDK WHAT FORMAT THIS IS", userid="user1")
+        m2 = Message(id=2, text="testmessage 2", time="IDK WHAT FORMAT THIS IS", userid="user2")
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(m1)
+        db.session.add(m2)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
 
