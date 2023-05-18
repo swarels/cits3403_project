ef8061359f14620875852428c57b6922c7dad5a4
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-18 15:54:50 +0800

    prep for merge

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, redirect, render_template, flash
 from app import app
 from flask_login import current_user, login_user, logout_user
+from flask_socketio import join_room, leave_room, emit, send, SocketIO
 from app.models import User
 from app.forms import LoginForm
 #from app.forms import SignUpForm
@@ -26,3 +27,47 @@ def logout():
 @app.route("/room")
 def room():
     return render_template("room.html")
+
+#@app.route("/signup", methods=["GET", "POST"])
+#def signup():
+#    username = None
+#    password = None
+#    form = SignUpForm()
+#    # Validate form
+#    if form.validate_on_submit():
+#        username = form.username.data
+#        form.username.data = ""
+
+    # if form.is_submitted():
+    #     result = request.form
+    #     return
+#    return render_template("signup.html", username=username, password=password, form=form)
+
+@app.route('/chatroom')
+def chatroom():
+    return render_template('chatroom.html')
+
+@app.route('/preferredname')
+def preferred_name():
+    return render_template('preferredname.html')
+
+@app.route("/talkingRat")
+def talkingRat():
+    return render_template("talkingRat.html")
+
+@app.route("/goalSetting")
+def goalSetting():
+    return render_template("goalSetting.html")
+
+@socketio.on("connect")
+def handle_connect(preferred_name):
+    print("Client connected!")
+
+@socketio.on("join_user")
+def connect_user(preferred_name):
+    print(f"User {preferred_name} joined")
+    socketio.emit("join_user", preferred_name)
+
+@socketio.on("chat")
+def new_message(data):
+    socketio.emit("chat", data)
