4ef71f33c4887d62b8b06de0f7d7f8ea10cfc91b
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-18 19:56:16 +0800

    transferred formatting etc from database branch

--- main.py
+++ main.py
@@ -2,85 +2,34 @@
 # pip install flask
 # pip install flask-socketio
 # pip install flask-wtf
-# pip install pymysql
-# pip install mysql-connector-python
+# pip install flask-sqlalchemy
+# pip install flask-migrate
+# pip install flask-login
 
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, request, session, redirect
 from flask_socketio import join_room, leave_room, emit, send, SocketIO
 import random
 from string import ascii_uppercase
+import os
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import TextAreaField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
-from forms import LoginForm
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from forms import LoginForm
-from forms import SignUpForm
-
-import mysql.connector
-
-
-"""
-Initialize flask object
-"""
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secretkey123"
+
+from app import app, db
+from app.models import User, Message
+
 socketio = SocketIO(app)
 
-# """
-# Initialize the database
-# """
-# mydb = mysql.connector.connect(
-#     host = "localhost",
-#     user="root",
-#     password="23073177"
-# )
-# # print(mydb) # tests the connection
-# mycurser = mydb.cursor()
-# mycurser.execute("CREATE DATABASE GymUsers")
-
-
-"""
-Main homepage
-"""
-
-
-@app.route("/", methods=["POST", "GET"])
-def home():
-    return render_template("home.html")
-
-
-"""
-Signup page
-"""
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    username = None
-    password = None
-    form = SignUpForm()
-    # Validate form
-    if form.validate_on_submit():
-        username = form.username.data
-        form.username.data = ""
-
-    # if form.is_submitted():
-    #     result = request.form
-    #     return
-    return render_template("signup.html", username=username, password=password, form=form)
-
-@app.route('/chatroom')
-def chatroom():
-    return render_template('chatroom.html')
-
-@app.route('/preferredname')
-def preferred_name():
-    return render_template('preferredname.html')
-
-@app.route("/talkingRat")
-def talkingRat():
-    return render_template("talkingRat.html")
-
-@app.route("/goalSetting")
-def goalSetting():
-    return render_template("goalSetting.html")
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Message': Message}
 
 @socketio.on("connect")
 def handle_connect(preferred_name):
@@ -94,7 +43,6 @@ def connect_user(preferred_name):
 @socketio.on("chat")
 def new_message(data):
     socketio.emit("chat", data)
-
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
