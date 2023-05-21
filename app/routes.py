5d907b6980e01660fcf0eeb2e97a5e2b0c150f45
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-21 22:36:12 +0800

    removed script which prevents default

--- app/routes.py
+++ app/routes.py
@@ -16,9 +16,8 @@ def home():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect('/index')
         login_user(user, remember=form.remember.data)
-        return redirect('/talkingRat')
+        return redirect('/chatroom')
     return render_template("home.html", title='Sign In', form=form)
 
 @app.route('/logout')
