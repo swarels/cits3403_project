0de20747cdcca23ee60fcacb424db761e480f0cc
emgreye <JakeRMcAuliffe@gmail.com>
2023-05-21 14:49:37 +0800

    Goal setting (almost) commits to db

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 #adapted from lecture 9
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, InputRequired, NumberRange, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -22,3 +22,33 @@ class SignUpForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Username is already in use')
+
+class GoalForm(FlaskForm):
+    goal = SelectField('Fitness Goal', [ InputRequired()],
+        choices=[ (-1, ''), (0, 'Lose weight'),
+        (1, 'Build Muscle'),
+        (2, 'Get in shape'),
+        (3, 'Other') ])
+    height = IntegerField('Height', [ InputRequired(),
+        NumberRange(min=50, max=250, message="You are too tall/short")
+        ])
+    weight = IntegerField('Weight', [ InputRequired(),
+        NumberRange(min=20, max=400, message="You are too heavy/thin")
+        ])
+    gender = SelectField('Gender', [ InputRequired()],
+        choices=[ (-1, ''), (0, 'Male'),
+        (1, 'Female'),
+        (2, 'Other')])
+    pastexercise = SelectField('Past Exercise', [ InputRequired()],
+        choices=[ (-1, ''), (0, 'Multiple times a week'),
+        (1, 'Once a Week or so'),
+        (2, 'On the off occasion'),
+        (3, 'Never') ])
+    willing = IntegerField('Willing', [ InputRequired(),
+        NumberRange(min=1, max=7, message="Invalid amount of days per week")
+        ])
+    allergies = StringField('Allergies', [ Length(min=1, max=128, message="Too much information")
+        ])
+    comments = StringField('Extra Info', [ Length(min=1, max=256, message="Too much information")
+        ])
+    submit = SubmitField('Submit')
